515132258476354a6e4184685fc301fa3950dfc2
unknown <sanjoy.wong@gmail.com>
2022-10-07 16:58:03 +0800

    Modifier inclusion function

--- functions/inclusionIncFile.php
+++ functions/inclusionIncFile.php
@@ -1,20 +1,13 @@
 
 <?php
-<<<<<<< HEAD
-// Exemple de $defaultPage : liste/listeEleve, login, planning/planningEleve
-=======
 // Exemple de $defaultPage :salon/, login
->>>>>>> upstream/developpe
 function inclusionIncFile($defaultPage){
     $files = array_merge(glob('./includes/*/*.inc.php'),glob('./includes/*.inc.php'));;
     $page = $_GET['page'] ?? $defaultPage;
     $pageTest = './includes/' . $page .'.inc.php';
 
-<<<<<<< HEAD
-=======
    
 
->>>>>>> upstream/developpe
     if (in_array($pageTest,$files))
     {
         require "./includes/$page.inc.php";
@@ -22,11 +15,7 @@ function inclusionIncFile($defaultPage){
     else 
     {
         require "./includes/$defaultPage.inc.php";
-<<<<<<< HEAD
-    } 
-=======
     }  
->>>>>>> upstream/developpe
 }
 
     ?>
