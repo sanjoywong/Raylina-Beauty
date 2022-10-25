10cd96144d2f44dc4f613cac802fe1470186c56e
sanjoy.wong <sanjoy.wong@gmail.com>
2022-10-25 10:32:12 +0200

    Modifier le css

--- includes/head.php
+++ includes/head.php
@@ -4,13 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Raylina Beauty</title>
-    
-<?php
-    loadCss(); 
-?>
-=======
     <link rel="stylesheet" media="screen" href="./assets/css/style.css" />
     <title>ceppic</title>
     <link
@@ -24,6 +17,5 @@
      <?php 
      loadCss();
      ?>
->>>>>>> upstream/developpe
 </head>
     
