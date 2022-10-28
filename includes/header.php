f46b3b7042db328b5187152054efa5d8153daf17
sane cheikh oumar <safietou2717@gmail.com>
2022-10-28 11:35:35 +0200

    prestations femme

--- includes/header.php
+++ includes/header.php
@@ -3,15 +3,15 @@
         
             <div> Raylina Beauty </div>
                <ul id="headerSalon">
-                        <li><a href=index.php?page=salon>Salon</a></li>
-                        <li><a href=index.php?page=soins>Soins</a></li>
-                        <li><a href=index.php?page=coiffure>Coiffure</a></li>
-                        <li><a href=index.php?page=barbier>Barbier</a></li>
-                        <li><a href=index.php?page=produits>Produits</a></li>
-                        <li><a href=index.php?page=tarifs>Tarifs</a></li>
-                        <li><a href=index.php?page=reservation>Reservation</a></li>
-                        <li><a href=index.php?page=contact>Contact</a></li>
-                        <li><a href=index.php?page=login>Login</a></li>
+                        <li><a href=index.php?page=salon>SALON</a></li>
+                        <!-- <li><a href=index.php?page=soins>SOINS</a></li> -->
+                        <li><a href=index.php?page=prestation-femme>PRESTATIONS FEMME</a></li>
+                        <li><a href=index.php?page=prestation-homme>PRESTATIONS HOMME</a></li>
+                        <li><a href=index.php?page=produits>PRODUITS</a></li>
+                        <!-- <li><a href=index.php?page=tarifs>TARIFS</a></li> -->
+                        <li><a href=index.php?page=reservation>RESERVATION</a></li>
+                        <li><a href=index.php?page=contact>CONTACT</a></li>
+                        <li><a href=index.php?page=login>LOGIN</a></li>
                </ul>
             </div>
            </nav>
