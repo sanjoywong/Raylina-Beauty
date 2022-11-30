cec66def69d044c4f6b2d784101c98eea6485c0b
sane cheikh oumar <safietou2717@gmail.com>
2022-11-30 12:19:18 +0100

    facture

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,4 +1,10 @@
-<?php 
+<link rel="stylesheet" href="css/rgpd.css">
+	<link rel="stylesheet" href="css/rgpd.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js"></script>
+        <script type="text/javascript" src="js/rgpd.js"></script><?php 
 
 $querySelect = new Sql();
 $requete = "SELECT * FROM `admin-salon` ;";
@@ -68,4 +74,78 @@ include './includes/frmLogin.php';
 
 
 
-?>
+?> <div id="bandeau_cookie" class="cookie_deactivate">
+<div class="row">
+    <div class="col-md-12 col-xs-12 cookie_spacer">
+        <span class="cookie_text">Ce site utilise des cookies afin que vous puissiez avoir la meilleure expérience
+            de navigation possible. En poursuivant votre navigation ou en cliquant sur l'option "ACCEPTER", vous
+            consentez à son utilisation.</span>
+
+    </div>
+    <div class="col-md-12 col-xs-12 container_cookie-btn">
+        <button id="acceptCookies" class="btn btn-advanced"> Accepter </button>
+        <button id="personnalize_my_cookie" type="button" class="btn btn-primary" data-toggle="modal"
+            data-target="#rgpd_modal">
+            Personnaliser
+        </button>
+        <a href="index.php?page=frmrgpd" class="btn btn-advanced">En savoir plus</a>
+    </div>
+
+</div>
+</div>
+
+<div class="modal fade" id="rgpd_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Gestionnaire des cookies</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="row ">
+                <div class="col-md-12 gestion_cookie-div-text">
+                    <h2>Je personnalise mes préférences</h2>
+                    <p>Les fonctionnalités de ce site listées ci-dessous s’appuient sur des services proposés par
+                        des tiers.
+                        Si vous donnez votre accord (consentement) ces cookies, ces tiers collecteront et
+                        utiliseront vos données de navigation pour des finalités qui leur sont propres, conformément
+                        à notre <a href="/rgpd">politique de confidentialité</a>.
+                        Cette page vous permet de donner ou de retirer votre consentement, soit globalement soit
+                        finalité par finalité.</p>
+                </div>
+            </div>
+            <div class="separator_section"></div>
+
+            <div class="row gestion_cookie-div">
+
+                <div class="col-md-6 text-left">
+                    <h2>
+                        Google Analystics
+                    </h2>
+                    <small>
+                        Permet de récolter des statistiques de fréquentation du site. Ces données sont anonymes et
+                        nous permettent d'améliorer votre expérience utilisateur.
+                    </small>
+                </div>
+                <div class="col-md-6">
+                    <button id="GTMAllow" class="deny accept btn_cookie" data-authorize="true"
+                        data-name="Analytics">
+                        J'autorise
+                    </button>
+                    <button id="GTMDenied" class="deny refuse btn_cookie" data-authorize="false"
+                        data-name="Analytics">
+                        Je refuse
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button id="close_modal" type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+            <button id="acceptcookies_modal" type="button" class="btn btn-primary">Confirmer mes choix</button>
+        </div>
+    </div>
+</div>
+</div>
--- includes/prestation-homme.inc.php
+++ includes/prestation-homme.inc.php
@@ -14,17 +14,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
-            integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
-            crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
-            integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
-            crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js"></script>
-        <script type="text/javascript" src="js/rgpd.js"></script>
+       
+     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js"></script>
+<script type="text/javascript" src="js/rgpd.js"></script>
 
 
 	
@@ -276,21 +268,75 @@ body{--wp--preset--color--black: #000000;--wp--preset--color--cyan-bluish-gray: 
 
 
 
-<div class="wp-block-getwid-accordion__header-wrapper"><h5 class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">Cire &amp; Fil</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></h5></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content">
-<div class="wp-block-uagb-advanced-heading uagb-block-26c4a52c uagb-heading-text"><h2 class="uagb-heading-text">Cire &amp; fil</h2><div class="uagb-separator-wrap"><div class="uagb-separator"></div></div><p class="uagb-desc-text"></p></div>
+<div class="wp-block-getwid-accordion__header-wrapper">
+	<h5 class="wp-block-getwid-accordion__header">
+		<a href="#"><span class="wp-block-getwid-accordion__header-title">Cire &amp; Fil</span>
+		<span class="wp-block-getwid-accordion__icon is-active">
+			<i class="fas fa-plus">
+</i></span><span class="wp-block-getwid-accordion__icon is-passive">
+			<i class="fas fa-minus"></i></span></a></h5></div>
+			<div class="wp-block-getwid-accordion__content-wrapper">
+				<div class="wp-block-getwid-accordion__content">
+<div class="wp-block-uagb-advanced-heading uagb-block-26c4a52c uagb-heading-text"><h2 class="uagb-heading-text">Cire &amp; fil</h2><div class="uagb-separator-wrap">
+	<div class="uagb-separator"></div></div><p class="uagb-desc-text"></p></div>
 
 
 
 <div class="wp-block-uagb-restaurant-menu uagb-rest_menu__outer-wrap uagb-block-10a8c65b">
-<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rest_menu__outer-wrap uagb-block-49b01b36"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rm__content"><div class="uagb-rm__text-wrap"><div class="uagb-rm-details"><div class="uagb-rm__title-wrap"><h4 class="uagb-rm__title">Structuration sourcils</h4><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc">au fil ou à la cire</div></div></div><div class="uagb-rm__price-wrap"><span class="uagb-rm__price">8€</span></div></div></div></div><div class="uagb-rm__separator-parent"><div class="uagb-rm__separator"></div></div></div></div></div>
+<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+	<div class="uagb-rest_menu__outer-wrap uagb-block-49b01b36"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+		<div class="uagb-rm__content">
+			<div class="uagb-rm__text-wrap">
+				<div class="uagb-rm-details">
+					<div class="uagb-rm__title-wrap">
+						<h4 class="uagb-rm__title">Structuration sourcils</h4>
+						<div class="uagb-rest-menu-text-wrap">
+							<div class="uagb-rm__desc">au fil ou à la cire</div>
+						</div></div>
+						<div class="uagb-rm__price-wrap">
+							<span class="uagb-rm__price">8€</span></div>
+						</div></div></div>
+						<div class="uagb-rm__separator-parent">
+							<div class="uagb-rm__separator">
+
+							</div></div></div></div></div>
 
 
 
-<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rest_menu__outer-wrap uagb-block-b691780b"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rm__content"><div class="uagb-rm__text-wrap"><div class="uagb-rm-details"><div class="uagb-rm__title-wrap"><h4 class="uagb-rm__title">Traçage barbe</h4><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc">au fil ou à la cire</div></div></div><div class="uagb-rm__price-wrap"><span class="uagb-rm__price">15€</span></div></div></div></div><div class="uagb-rm__separator-parent"><div class="uagb-rm__separator"></div></div></div></div></div>
+<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+	<div class="uagb-rest_menu__outer-wrap uagb-block-b691780b"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+		<div class="uagb-rm__content">
+			<div class="uagb-rm__text-wrap">
+				<div class="uagb-rm-details">
+					<div class="uagb-rm__title-wrap">
+						<h4 class="uagb-rm__title">Traçage barbe</h4>
+						<div class="uagb-rest-menu-text-wrap">
+							<div class="uagb-rm__desc">au fil ou à la cire</div>
+						</div></div>
+						<div class="uagb-rm__price-wrap">
+							<span class="uagb-rm__price">15€</span></div>
+						</div></div></div><div class="uagb-rm__separator-parent">
+							<div class="uagb-rm__separator"></div>
+						</div>
+					</div></div></div>
 
 
 
-<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rest_menu__outer-wrap uagb-block-57fbb670"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rm__content"><div class="uagb-rm__text-wrap"><div class="uagb-rm-details"><div class="uagb-rm__title-wrap"><h4 class="uagb-rm__title">Epilation nez et oreilles</h4><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc"></div></div></div><div class="uagb-rm__price-wrap"><span class="uagb-rm__price">10€</span></div></div></div></div><div class="uagb-rm__separator-parent"><div class="uagb-rm__separator"></div></div></div></div></div>
+<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+	<div class="uagb-rest_menu__outer-wrap uagb-block-57fbb670">
+		<div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+			<div class="uagb-rm__content">
+				<div class="uagb-rm__text-wrap">
+					<div class="uagb-rm-details">
+						<div class="uagb-rm__title-wrap">
+							<h4 class="uagb-rm__title">Epilation nez et oreilles</h4>
+							<div class="uagb-rest-menu-text-wrap">
+								<div class="uagb-rm__desc"></div>
+							</div></div><div class="uagb-rm__price-wrap">
+								<span class="uagb-rm__price">10€</span></div>
+							</div></div></div><div class="uagb-rm__separator-parent">
+								<div class="uagb-rm__separator"></div>
+							</div></div></div></div>
 </div>
 </div></div>
 
@@ -484,11 +530,55 @@ body{--wp--preset--color--black: #000000;--wp--preset--color--cyan-bluish-gray: 
 
 
 
-<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rest_menu__outer-wrap uagb-block-99983e15"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rm__content"><div class="uagb-rm__text-wrap"><div class="uagb-rm-details"><div class="uagb-rm__title-wrap"><h4 class="uagb-rm__title">Coloration de barbe</h4><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc"></div></div></div><div class="uagb-rm__price-wrap"><span class="uagb-rm__price">+ 15€</span></div></div></div></div><div class="uagb-rm__separator-parent"><div class="uagb-rm__separator"></div></div></div></div></div>
+<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+	<div class="uagb-rest_menu__outer-wrap uagb-block-99983e15">
+		<div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+			<div class="uagb-rm__content"><div class="uagb-rm__text-wrap">
+				<div class="uagb-rm-details">
+					<div class="uagb-rm__title-wrap">
+					<h4 class="uagb-rm__title">Coloration de barbe</h4>
+					<div class="uagb-rest-menu-text-wrap">
+						<div class="uagb-rm__desc">
+
+					</div>
+				</div>
+			</div>
+			<div class="uagb-rm__price-wrap">
+				<span class="uagb-rm__price">+ 15€</span>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="uagb-rm__separator-parent">
+	<div class="uagb-rm__separator">
+		
+	</div></div></div></div></div>
 
 
 
-<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rest_menu__outer-wrap uagb-block-0241a99e"><div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 "><div class="uagb-rm__content"><div class="uagb-rm__text-wrap"><div class="uagb-rm-details"><div class="uagb-rm__title-wrap"><h4 class="uagb-rm__title">masque supplémentaire</h4><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc"></div></div></div><div class="uagb-rm__price-wrap"><span class="uagb-rm__price">+ 8€</span></div></div></div></div><div class="uagb-rm__separator-parent"><div class="uagb-rm__separator"></div></div></div></div></div>
+<div class="uagb-rest_menu__wrap uagb-rest_menu_repeater  uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+	<div class="uagb-rest_menu__outer-wrap uagb-block-0241a99e">
+		<div class=" uagb-rm__align-left uagb-rm__desk-column-1 uagb-rm__tablet-column-2 uagb-rm__mobile-column-1 ">
+			<div class="uagb-rm__content"><div class="uagb-rm__text-wrap">
+				<div class="uagb-rm-details"><div class="uagb-rm__title-wrap">
+					<h4 class="uagb-rm__title">masque supplémentaire</h4
+					><div class="uagb-rest-menu-text-wrap"><div class="uagb-rm__desc">
+
+					</div></div></div>
+					<div class="uagb-rm__price-wrap">
+						<span class="uagb-rm__price">+ 8€</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="uagb-rm__separator-parent">
+	<div class="uagb-rm__separator">
+
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 </div>
 </div>
